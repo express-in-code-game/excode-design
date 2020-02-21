e39333a4faa11f8d05d9dbf41efd28315bf2d2c6
seeris <sergei.udris@gmail.com>
2020-02-21 07:34:07 +0300

    wip

--- app/src/app/kafka/streams_example.clj
+++ app/src/app/kafka/streams_example.clj
@@ -17,7 +17,10 @@
    org.apache.kafka.clients.producer.ProducerRecord
    org.apache.kafka.streams.kstream.ValueMapper
    org.apache.kafka.streams.kstream.KeyValueMapper
+   org.apache.kafka.streams.kstream.Materialized
+   org.apache.kafka.streams.kstream.Produced
    java.util.ArrayList
+   java.util.Locale
    java.util.Arrays))
 
 ; https://kafka.apache.org/24/documentation/streams/tutorial
@@ -236,6 +239,114 @@
   ; Streams application: statefull wordcount
   ; https://kafka.apache.org/24/documentation/streams/tutorial#tutorial_code_wordcount
 
+  (do
+    (def props (Properties.))
+
+    (.put props "bootstrap.servers" "kafka1:9092")
+    (.put props "group.id" "test")
+    (.put props "log.cleanup.policy" "compact")
+    (.put props "cleanup.policy" "compact")
+    (.put props "enable.auto.commit" "false")
+    (.put props "key.deserializer" "org.apache.kafka.common.serialization.StringDeserializer")
+    (.put props "value.deserializer" "org.apache.kafka.common.serialization.StringDeserializer")
+
+    (def client (AdminClient/create props)))
+  
+  (do
+    ; async, cannot be executed within do block
+    #_(.deleteTopics client (java.util.ArrayList. ["streams-wordcount-input"
+                                                   "streams-wordcount-output"]))
+    (def topics (java.util.ArrayList.
+                 [(NewTopic. "streams-wordcount-input" 1 (short 1))
+                  (NewTopic. "streams-wordcount-output" 1 (short 1))]))
+    (.createTopics client topics)
+    )
+
+
+  (do
+    (def props (Properties.))
+
+    (.put props StreamsConfig/APPLICATION_ID_CONFIG "streams-wordcount")
+    (.put props StreamsConfig/BOOTSTRAP_SERVERS_CONFIG "kafka1:9092")
+    (.put props StreamsConfig/DEFAULT_KEY_SERDE_CLASS_CONFIG (.. Serdes String getClass))
+    (.put props StreamsConfig/DEFAULT_VALUE_SERDE_CLASS_CONFIG (.. Serdes String getClass))
+
+    (def builder (StreamsBuilder.))
+    (def ^KStream source (.stream builder "streams-wordcount-input"))
+    (-> source
+        (.flatMapValues
+         (reify ValueMapper
+           (apply [this vl]
+             (Arrays/asList (-> vl (.toLowerCase (Locale/getDefault)) (.split "\\W+"))))))
+        (.groupBy
+         (reify KeyValueMapper
+           (apply [this k vl]
+             vl)))
+        (.count (Materialized/as "counts-store"))
+        (.toStream)
+        (.to "streams-wordcount-output" (Produced/with (Serdes/String) (Serdes/Long))))
+
+    (def topology (.build builder))
+
+    (println (.describe topology))
+
+    (def streams (KafkaStreams. topology props))
+
+    (def latch (CountDownLatch. 1))
+
+    (-> (Runtime/getRuntime)
+        (.addShutdownHook (proxy
+                           [Thread]
+                           ["streams-shutdown-hook"]
+                            (run []
+                              (.println (System/out) "--closing stream")
+                              (.close streams)
+                              (.countDown latch))))))
+
+  (def fu-streams
+    (future-call
+     (fn []
+       (try
+         (do
+           (.start streams)
+           #_(.await latch)) ; .await latch halts
+         (catch Exception e (.println System/out (str "caught e: " (.getMessage e))))))))
+
+  (future-cancel fu-streams)
+  (.close streams)
+
+  (def fu-consumer
+    (future-call (fn []
+                   (do
+                     (def consumer (KafkaConsumer.
+                                    {"bootstrap.servers" "kafka1:9092"
+                                     "auto.offset.reset" "earliest"
+                                     "auto.commit.enable" "false"
+                                     "group.id" (.toString (java.util.UUID/randomUUID))
+                                     "consumer.timeout.ms" "5000"
+                                     "key.deserializer" "org.apache.kafka.common.serialization.StringDeserializer"
+                                     "value.deserializer" "org.apache.kafka.common.serialization.LongDeserializer"}))
+                     
+                     (.subscribe consumer (Arrays/asList (object-array ["streams-wordcount-output"]))))
+
+                   (while true
+                     (let [records (.poll consumer 1000)]
+                       (.println System/out (str "polling records:" (java.time.LocalTime/now)))
+                       (doseq [rec records]
+                         (prn (str (.key rec) " : " (.value rec)))))))))
+
+  (future-cancel fu-consumer)
+
+  (def producer (KafkaProducer.
+                 {"bootstrap.servers" "kafka1:9092"
+                  "value.serializer" "org.apache.kafka.common.serialization.StringSerializer"
+                  "key.serializer" "org.apache.kafka.common.serialization.StringSerializer"}))
+
+  ; why takes too long to process ..?
+  
+  (.send producer (ProducerRecord.
+                   "streams-wordcount-input"
+                   (.toString (java.util.UUID/randomUUID)) "all streams lead to kafka"))
 
 
   ;
