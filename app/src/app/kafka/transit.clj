356f6c4d59c701bfefbcceb2ed882e34a03c40d1
seeris <sergei.udris@gmail.com>
2020-02-22 10:05:31 +0300

    wip

--- app/src/app/kafka/transit.clj
+++ app/src/app/kafka/transit.clj
@@ -58,47 +58,47 @@
 (comment
 
   (create-topic {:conf base-conf
-                 :name "transit.input"
-                 :num-partitions 1
-                 :replication-factor 1})
-
-  (create-topic {:conf base-conf
-                 :name "transit.output"
+                 :name "transit-input"
                  :num-partitions 1
                  :replication-factor 1})
 
   (list-topics {:conf base-conf})
 
   (delete-topics {:conf base-conf
-                  :names ["transit.input"
-                          "transit.output"]})
+                  :names ["transit-input"]})
 
   (def fu-consumer
-    (future-call
-     (fn []
-       (let [conf {"bootstrap.servers" (get base-conf "bootstrap.servers")
-                   "group.id" "test"
-                   "enable.auto.commit" "false"
-                   "key.deserializer" "org.apache.kafka.common.serialization.StringDeserializer"
-                   "value.deserializer" "app.kafka.serdes.TransitJsonDeserializer"}
-             consumer (KafkaConsumer. conf)]
-         (.subscribe consumer (Arrays/asList (object-array ["transit.input"])))
-         (while true
-           (let [records (.poll consumer 1000)]
-             (.println System/out (str "polling records " (java.time.LocalTime/now)))
-             (doseq [rec records]
-               (prn (str (.key rec) " " (.value rec))))))))))
-  
+    (future-call (fn []
+                   (let [consumer (KafkaConsumer.
+                                   {"bootstrap.servers" "broker1:9092"
+                                    "auto.offset.reset" "earliest"
+                                    "auto.commit.enable" "false"
+                                    "group.id" (.toString (java.util.UUID/randomUUID))
+                                    "consumer.timeout.ms" "5000"
+                                    "key.deserializer" "org.apache.kafka.common.serialization.StringDeserializer"
+                                    "value.deserializer"
+                                    #_"org.apache.kafka.common.serialization.StringDeserializer"
+                                    "app.kafka.serdes.TransitJsonDeserializer"})]
+                     (.subscribe consumer (Arrays/asList (object-array ["transit-input"])))
+                     (while true
+                       (let [records (.poll consumer 1000)]
+                         (.println System/out (str "polling records:" (java.time.LocalTime/now)))
+                         (doseq [rec records]
+                           (prn (str (.key rec) " : " (.value rec))))))))))
+
   (future-cancel fu-consumer)
 
   (def producer (KafkaProducer.
                  {"bootstrap.servers" "broker1:9092"
+                  "auto.commit.enable" "true"
                   "key.serializer" "org.apache.kafka.common.serialization.StringSerializer"
-                  "value.serializer" "app.kafka.serdes.TransitJsonSerializer"}))
+                  "value.serializer"
+                  #_"org.apache.kafka.common.serialization.StringSerializer"
+                  "app.kafka.serdes.TransitJsonSerializer"}))
 
   (.send producer (ProducerRecord.
-                   "transit.input"
-                   (.toString (java.util.UUID/randomUUID)) {:a 1 :b 2}))
+                   "transit-input"
+                   (.toString (java.util.UUID/randomUUID)) {:a 2}))
 
   ;
   )
