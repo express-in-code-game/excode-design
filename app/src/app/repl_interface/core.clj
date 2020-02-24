044545e71fb8996454a6a5a14b628abf88eb9908
seeris <sergei.udris@gmail.com>
2020-02-24 07:49:04 +0300

    :step KGroupedStream reduce using transit and (merge ag v)

--- app/src/app/repl_interface/core.clj
+++ app/src/app/repl_interface/core.clj
@@ -23,6 +23,13 @@
    org.apache.kafka.streams.kstream.KeyValueMapper
    org.apache.kafka.streams.kstream.Materialized
    org.apache.kafka.streams.kstream.Produced
+   org.apache.kafka.streams.kstream.Reducer
+   org.apache.kafka.streams.kstream.Grouped
+   org.apache.kafka.streams.state.QueryableStoreTypes
+
+   org.apache.kafka.streams.kstream.Initializer
+   org.apache.kafka.streams.kstream.Aggregator
+
    java.util.ArrayList
    java.util.Locale
    java.util.Arrays))
@@ -91,7 +98,7 @@
   (let [client (KafkaAdminClient/create conf)
         topics (java.util.ArrayList.
                 (mapv (fn [name]
-                        [(NewTopic. name num-partitions (short replication-factor))]) names))]
+                        (NewTopic. name num-partitions (short replication-factor))) names))]
     (.createTopics client topics)))
 
 (defn delete-topics
@@ -161,6 +168,77 @@
   (delete-topics {:conf base-conf
                   :names topic-names})
 
+  (def builder (StreamsBuilder.))
+
+  (def ktable (-> builder
+                  (.stream "user.data")
+                  (.groupByKey)
+                  (.reduce (reify
+                             Reducer
+                             (apply [this ag vl]
+                               (merge ag vl)))
+                           (-> (Materialized/as "user.data.store")
+                               (.withKeySerde (Serdes/String))
+                               (.withValueSerde (TransitJsonSerde.))))))
+
+  (def topology (.build builder))
+
+  (println (.describe topology))
+
+  (def streams (KafkaStreams.
+                topology
+                (doto (Properties.)
+                  (.putAll {"application.id" "user.data"
+                            "bootstrap.servers" "broker1:9092"
+                            "default.key.serde" (.. Serdes String getClass)
+                            "default.value.serde" "app.kafka.serdes.TransitJsonSerde"}))))
+
+  (def latch (CountDownLatch. 1))
+
+  (add-shutdown-hook streams latch)
+
+  (def fu-streams
+    (future-call
+     (fn []
+       (.start streams)
+       #_(.await latch) ; halts
+       )))
+
+  (future-cancel fu-streams)
+  (.close streams)
+
+  (def producer (KafkaProducer.
+                 {"bootstrap.servers" "broker1:9092"
+                  "auto.commit.enable" "true"
+                  "key.serializer" "org.apache.kafka.common.serialization.StringSerializer"
+                  "value.serializer" "app.kafka.serdes.TransitJsonSerializer"}))
+
+  (def users {0 (.toString #uuid "5ada3765-0393-4d48-bad9-fac992d00e62")
+              1 (.toString #uuid "179c265a-7f72-4225-a785-2d048d575854")
+              2 (.toString #uuid "3a3e2d06-3719-4811-afec-0dffdec35543")})
+
+  (.send producer (ProducerRecord.
+                   "user.data"
+                   (get users 0)
+                   {:email "user0@gmail.com"
+                    :username "user0"}))
+
+  (.send producer (ProducerRecord.
+                   "user.data"
+                   (get users 1)
+                   {:email "user1@gmail.com"
+                    :username "user1"}))
+
+  (.send producer (ProducerRecord.
+                   "user.data"
+                   (get users 2)
+                   {:email "user2@gmail.com"
+                    :username "user2"}))
+
+  (def view (.store streams "user.data.store" (QueryableStoreTypes/keyValueStore)))
+  (.get view (get users 0))
+  (.get view (get users 1))
+  (.get view (get users 2))
 
 
 
