75061ca3d745fbfe97f841306973478a530b4da2
seeris <sergei.udris@gmail.com>
2020-03-13 17:15:30 +0300

    ui csp

--- ui/src/starnet/ui/csp/main.cljs
+++ ui/src/starnet/ui/csp/main.cljs
@@ -16,7 +16,50 @@
    [starnet.ui.alpha.tests]))
 
 
-
 (defn ^:export main
   []
   (println "csp"))
+
+(def chan-1 (chan (a/sliding-buffer 10)))
+(def chan-1-pub (pub chan-1 first))
+(def view-1 (atom {}))
+
+(defn proc-main
+  [p out]
+  (let [c (chan 1)]
+    (sub p :main c)
+    (go (loop []
+          (when-let [[[t v] c] (alts! [c])]
+            (condp = v
+              :init (do (println "initilized"))))
+          (recur))
+        (println "closing go block: proc-main"))
+    c))
+
+(defn proc-render-containers
+  [p out]
+  (let [c (chan 1)]
+    (sub p :main c)
+    (go (loop []
+          (when-let [[[t v] c] (alts! [c])]
+            (condp = v
+              :init (do (r/render [:<>
+                                   [:div {:id "div-1"}]
+                                   [:div {:id "div-2"}]
+                                   [:div {:id "div-3"}]]
+                                  (.getElementById js/document "ui"))
+                        (println "rendered containers")
+                        )))
+          (recur))
+        (println "closing go block: proc-render-containers"))
+    c))
+
+(proc-main chan-1-pub chan-1)
+(proc-render-containers chan-1-pub chan-1)
+
+(comment
+
+  (put! chan-1 [:main :init])
+
+  ;;
+  )
