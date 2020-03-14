66af40198d18e36618537a87b29d5594749a21eb
seeris <sergei.udris@gmail.com>
2020-03-14 13:08:01 +0300

    :samples crux

--- app/test/starnet/app/crux_samples/core.clj
+++ app/test/starnet/app/crux_samples/core.clj
@@ -4,13 +4,15 @@
    [crux.api :as crux]
    [clojure.java.io :as io]))
 
+(def data-dir "/tmp/crux-store")
+
 (comment
 
   (defn start-standalone-node ^crux.api.ICruxAPI [storage-dir]
     (crux/start-node {:crux.node/topology '[crux.standalone/topology]
                       :crux.kv/db-dir (str (io/file storage-dir "db"))}))
 
-  (def node (start-standalone-node "/tmp/crux-store"))
+  (def node (start-standalone-node data-dir))
 
   (crux/submit-tx
    node
@@ -31,6 +33,36 @@
   #_{:crux.db/id :dbpedia.resource/Pablo-Picasso
      :name "Pablo"
      :last-name "Picasso"}
+
+  (.close node)
+  
+
+  ;;
+  )
+
+(comment
+
+  (def crux
+    (crux/start-node
+     {:crux.node/topology :crux.standalone/topology
+      :crux.node/kv-store "crux.kv.memdb/kv"
+      :crux.standalone/event-log-dir (str data-dir "/eventlog-1")
+      :crux.kv/db-dir (str data-dir "/db-dir")
+      :crux.standalone/event-log-kv-store "crux.kv.memdb/kv"}))
+
+  (def manifest
+    {:crux.db/id :manifest
+     :pilot-name "Johanna"
+     :id/rocket "SB002-sol"
+     :id/employee "22910x2"
+     :badges "SETUP"
+     :cargo ["stereo" "gold fish" "slippers" "secret note"]})
+  
+  (crux/submit-tx crux [[:crux.tx/put manifest]])
+  
+  (crux/entity (crux/db crux) :manifest)
+  
+  (.close crux)
 
 
   ;;
