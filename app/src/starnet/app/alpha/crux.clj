3eed38cd23498c97953986a2d19fbca3dbca45d6
seeris <sergei.udris@gmail.com>
2020-03-14 14:40:31 +0300

    :build crux deps

--- app/src/starnet/app/alpha/crux.clj
+++ app/src/starnet/app/alpha/crux.clj
@@ -4,6 +4,14 @@
    [crux.api :as crux]
    [clojure.java.io :as io]))
 
+(defn easy-ingest
+  "Uses Crux put transaction to add a vector of documents to a specified
+  node"
+  [node docs]
+  (crux/submit-tx node
+                  (vec (for [doc docs]
+                         [:crux.tx/put doc]))))
+
 (comment
 
   (def node (crux/start-node {:crux.node/topology '[crux.kafka/topology
@@ -17,6 +25,52 @@
                               :crux.kv/db-dir "/tmp/crux"
                               :crux.kv/sync? false
                               :crux.kv/check-and-store-index-version true}))
+
+
+  (easy-ingest
+   node
+   [{:crux.db/id :commodity/Pu
+     :common-name "Plutonium"
+     :type :element/metal
+     :density 19.816
+     :radioactive true}
+
+    {:crux.db/id :commodity/N
+     :common-name "Nitrogen"
+     :type :element/gas
+     :density 1.2506
+     :radioactive false}
+
+    {:crux.db/id :commodity/CH4
+     :common-name "Methane"
+     :type :molecule/gas
+     :density 0.717
+     :radioactive false}
+
+    {:crux.db/id :commodity/Au
+     :common-name "Gold"
+     :type :element/metal
+     :density 19.300
+     :radioactive false}
+
+    {:crux.db/id :commodity/C
+     :common-name "Carbon"
+     :type :element/non-metal
+     :density 2.267
+     :radioactive false}
+
+    {:crux.db/id :commodity/borax
+     :common-name "Borax"
+     :IUPAC-name "Sodium tetraborate decahydrate"
+     :other-names ["Borax decahydrate" "sodium borate" "sodium tetraborate" "disodium tetraborate"]
+     :type :mineral/solid
+     :appearance "white solid"
+     :density 1.73
+     :radioactive false}])
+
+  (crux/q (crux/db node)
+          '{:find [element]
+            :where [[element :type :element/metal]]})
 
   (.close node)
 
