c2728ea11a75b56fd446571616ce8c11c1536ced
seeris <sergei.udris@gmail.com>
2020-03-17 17:43:47 +0300

    :app create user in crux

--- app/src/starnet/app/alpha/core.clj
+++ app/src/starnet/app/alpha/core.clj
@@ -4,7 +4,13 @@
    [clojure.core.async :as a :refer [<! >! <!! timeout chan alt! go
                                      >!! <!! alt!! alts! alts!! take! put!
                                      thread pub sub]]
-   [clojure.java.io :as io]))
+   [clojure.java.io :as io]
+   [clojure.spec.alpha :as s]
+   [clojure.spec.gen.alpha :as sgen]
+   [clojure.spec.test.alpha :as stest]
+   [clojure.test.check.generators :as gen]
+   [clojure.test.check.properties :as prop]
+   [starnet.common.alpha.spec]))
 
 (defn authorized?
   [{:keys [ch-cruxdb] :as channels}])
@@ -18,6 +24,55 @@
     c-out))
 
 (defn create-user
-  [channels tx-data]
-  (db-tx channels tx-data))
+  [channels data]
+  (db-tx channels [[:crux.tx/put
+                    (-> data
+                        (merge {:crux.db/id (:u/uuid data)}))
+                    ;; #inst "2112-12-03"
+                    ;; #inst "2113-12-03"
+                    ]]))
 
+(defn repl-query
+  [channels query-data]
+  (let [c-out (chan 1)]
+    (put! (channels :ch-cruxdb) {:cruxdb/op :query
+                                 :ch/c-out c-out
+                                 :cruxdb/query-data query-data})
+    (first (alts!! [c-out (timeout 100)]))))
+
+(defn repl-tx
+  [channels tx-data]
+  (let [c-out (chan 1)]
+    (put! (channels :ch-cruxdb) {:cruxdb/op :tx
+                                 :ch/c-out c-out
+                                 :cruxdb/tx-data tx-data})
+    (first (alts!! [c-out (timeout 100)]))))
+
+(defn <!!soft
+  [c]
+  (first (alts!! [c (timeout 100)])))
+
+(comment
+
+  (def channels @(resolve 'starnet.app.alpha.main/channels))
+
+  ; all entities
+
+  (repl-query channels '{:find [id]
+                         :where [[e :crux.db/id id]]
+                         :full-results? true})
+
+  (->
+   (create-user channels (gen/generate (s/gen :u/user)))
+   (<!!soft))
+
+  (repl-query channels '{:find [id]
+                         :where [[e :u/uuid id]]
+                         :full-results? true})
+
+
+
+
+
+  ;;
+  )
