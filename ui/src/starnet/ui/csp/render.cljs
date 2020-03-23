25a203a95f9d6a2a731a0b8cbb4a770024b421a9
seeris <sergei.udris@gmail.com>
2020-03-23 14:05:36 +0300

    ui

--- ui/src/starnet/ui/csp/render.cljs
+++ ui/src/starnet/ui/csp/render.cljs
@@ -26,7 +26,6 @@
    ["antd/lib/divider" :default AntDivider]
    ["@ant-design/icons/SmileOutlined" :default AntSmileOutlined]))
 
-
 (def ant-row (r/adapt-react-class AntRow))
 (def ant-col (r/adapt-react-class AntCol))
 (def ant-divider (r/adapt-react-class AntDivider))
@@ -42,20 +41,55 @@
 (def ant-smile-outlined (r/adapt-react-class AntSmileOutlined))
 
 
-#_(defn ui-header
-    [channels state]
-    (let [{:keys [history/pushed]} state
-          {:keys [handler]} pushed]
-      [:header {:class "ui-header" :style {:display "flex"}}
-       [:div "starnet"]
-       [:a {:href "/events"} "events"]
-       [:br]
-       [:a {:href "/games"} "games"]
-       [:a {:href "u/games"} "u/games"]
-       [:a {:href "/settings"} "settings"]
-       [:a {:href (gstring/format "/u/%s" (gen/generate gen/string-alphanumeric))} "user/random"]
-       [:a {:href (gstring/format "/non-existing" (gen/generate gen/string-alphanumeric))} "not-found"]]))
+(declare rc-ui)
 
+(def ^:private ratoms (let []
+                        {:state (r/atom {})}))
+
+(defn proc-derived-state-ui
+  [{:keys [ml-router ch-derived-state-ui ml-http-res]}]
+  (let [c-router (chan 1)
+        c-http (chan 1)]
+    (tap ml-router c-router)
+    (tap ml-http-res  c-http)
+    (go (loop [s nil]
+          (if-let [[v port] (alts! [c-router])]
+            (condp = port
+              c-router (let [u (select-keys v [:router/handler :history/pushed])
+                             s (merge s u)]
+                         (println "proc-derived-state-ui" s)
+                         (put! ch-derived-state-ui s)
+
+                         (recur s)))))
+        (println "closing proc-derived-state-ui"))))
+
+(defn proc-render-ui
+  [{:keys [] :as channels}]
+  (let [c-dsu (chan 1)
+        root-el (.getElementById js/document "ui")]
+    (go (loop []
+          (let [{:keys [router/handler history/pushed] :as v} (<! c-dsu)]
+            (println (gstring/format "rendering %s" handler))
+            (condp = handler
+              :page/events (do
+                             (render/page-events root-el channels v)
+                             (recur))
+              :page/games (do
+                            (render/page-games root-el channels v)
+                            (recur))
+              :page/user-games (do
+                                 (render/page-user-games root-el channels v)
+                                 (recur))
+              :page/userid-games (do
+                                   (render/page-userid-games root-el channels v)
+                                   (recur))
+              :page/userid (do
+                             (render/page-userid root-el channels v)
+                             (recur))
+              (do
+                (render/not-found root-el channels v)
+                (recur)))))
+        (println "closing proc-render"))))
 
 
 (defn menu
@@ -83,7 +117,6 @@
         [:a {:href (gstring/format "/non-existing" (gen/generate gen/string-alphanumeric))} "non-existing"]]
        [ant-menu-item {:key :page/login}
         [:a {:href "/login"} "login"]]])))
-
 
 (defn layout
   [content]
@@ -115,272 +148,117 @@
     content]]
   )
 
+(defn rc-page-events
+  [channels ratoms]
+  (let []
+    (fn [channels ratoms]
+      (let []
+        [layout
+         [:<>
+          [:div "rc-page-events"]]]))))
 
-(defn render-page-events
-  [el channels state]
-  (rdom/render [layout
-             [:<>
-              [:div {:id "div-1"} "page events"]]
-             ]  el))
+(defn rc-page-settings
+  [channels ratoms]
+  (let []
+    (fn [channels ratoms]
+      (let []
+        [layout
+         [:<>
+          [:div "rc-page-settings"]]]))))
 
-(defn render-page-settings
-  [el channels state]
-  (rdom/render [layout
-             [:<>
-              [:div  "page settings"]]]  el))
+(defn rc-page-games
+  [channels ratoms]
+  (let []
+    (fn [channels ratoms]
+      (let []
+        [layout
+         [:<>
+          [:div "rc-page-games"]]]))))
 
-(defn render-page-games
-  [el channels state]
-  (rdom/render [layout
-             [:<>
-              [:div  "page games"]]]
-            el))
+(defn rc-page-userid-games
+  [channels ratoms]
+  (let []
+    (fn [channels ratoms]
+      (let []
+        [layout
+         [:<>
+          [:div "rc-page-userid-games"]]]))))
 
-(defn render-page-userid-games
-  [el channels state]
-  (rdom/render [layout
-             [:<>
-              [:div  "page user/name/games"]]]  el))
+(defn rc-page-userid
+  [channels ratoms]
+  (let []
+    (fn [channels ratoms]
+      (let []
+        [layout
+         [:<>
+          [:div "rc-page-userid"]]]))))
 
+(defn rc-page-not-found
+  [channels ratoms]
+  (let []
+    (fn [channels ratoms]
+      (let []
+        [layout
+         [:<>
+          [:div "rc-page-not-found"]]]))))
 
+(defn rc-page-login
+  [channels ratoms]
+  (let []
+    (fn [channels ratoms]
+      (let []
+        [layout
+         [:<>
+          [:div "rc-page-login"]]]))))
 
-(defn render-page-userid
-  [el channels state]
-  (rdom/render [layout
-             [:<>
-              [:div  "page userid"]]]  el))
+(defn rc-page-game
+  [channels ratoms]
+  (let []
+    (fn [channels ratoms]
+      (let []
+        [layout
+         [:<>
+          [:div "rc-page-game"]]]))))
 
-(defn render-not-found
-  [el channels state]
-  (rdom/render [layout
-             [:<>
-              [:div  "not found"]]]  el))
+(defn rc-page-user-games
+  [channels ratoms]
+  (let []
+    (fn [channels ratoms]
+      (let []
+        [layout
+         [:<>
+          [:div  "rc-page-user-games"]
+          [ant-button {:value "button" :size "small"} "button"]
+          [:div
+           [:a {:target "_blank"
+                :href (gstring/format "/game/%s" (str (gen/generate gen/uuid)))}
+            [ant-button {:size "small"} "create game"]]]]
+         ]))))
 
-(defn render-page-login
-  [el channels state]
-  (rdom/render [layout
-             [:<>
-              [:div  "page login"]]]  el))
+(defn rc-ui
+  [channels ratoms]
+  (let [{:keys [router/handler history/pushed]} @(ratoms :state)]
+    (fn [channels ratoms]
+      (println (gstring/format "rendering %s" handler))
+      (let []
+        (condp = handler
+          :page/events [rc-page-events channels ratoms]
+          :page/games [rc-page-games channels ratoms]
+          :page/user-games [rc-page-user-games channels ratoms]
+          :page/userid-games [rc-page-userid-games channels ratoms]
+          :page/userid [rc-page-userid channels ratoms]
+          [rc-page-not-found channels ratoms])))))
 
-(defn render-page-game
-  [el channels state]
-  (rdom/render [layout-game
-             [:<>
-              [:div  "page game"]]]  el))
-
-(defn render-page-user-games
-  [el channels state]
-  (rdom/render [layout
-             [:<>
-              [ant-button {:value "button" :size "small"} "button"]
-              [:div
-               [:a {:target "_blank"
-                    :href (gstring/format "/game/%s" (str (gen/generate gen/uuid)))}
-                [ant-button {:size "small"} "create game"]]]
-              [:div  "page u/games"]]]  el))
-
-(defn proc-page-user-games
-  [{:keys [ml-router ml-http-res] :as channels}]
-  (let [c-router (chan 1)
-        root-el (.getElementById js/document "ui")]
-    (tap ml-router  c-router)
-    (go (loop [state nil]
-          (let [[v port] (alts! [c-router])]
-            (condp = port
-              c-router (let [{:keys [router/handler history/pushed]} v]
-                         (if (= handler :page/user-games)
-                           (do
-                             #_(println (gstring/format "rendering %s" handler))
-                             (render-page-user-games root-el channels v)
-                             (recur (merge state v)))
-                           (do (recur state)))))))
-        (println "proc-page-user-games closing"))))
-
-(defn proc-page-events
-  [{:keys [ml-router ml-http-res] :as channels}]
-  (let [c-router (chan 1)
-        root-el (.getElementById js/document "ui")]
-    (tap ml-router  c-router)
-    (go (loop [state nil]
-          (let [[v port] (alts! [c-router])]
-            (condp = port
-              c-router (let [{:keys [router/handler history/pushed]} v]
-                         (if (= handler :page/events)
-                           (do
-                             #_(println (gstring/format "rendering %s" handler))
-                             (render-page-events root-el channels v)
-                             (recur (merge state v)))
-                           (do (recur state)))))))
-        (println "proc-page-events closing"))))
-
-(defn proc-page-settings
-  [{:keys [ml-router ml-http-res] :as channels}]
-  (let [c-router (chan 1)
-        root-el (.getElementById js/document "ui")]
-    (tap ml-router  c-router)
-    (go (loop [state nil]
-          (let [[v port] (alts! [c-router])]
-            (condp = port
-              c-router (let [{:keys [router/handler history/pushed]} v]
-                         (if (= handler :page/settings)
-                           (do
-                             #_(println (gstring/format "rendering %s" handler))
-                             (render-page-settings root-el channels v)
-                             (recur (merge state v)))
-                           (do (recur state)))))))
-        (println "proc-page-settings closing"))))
-
-(defn proc-page-games
-  [{:keys [ml-router ml-http-res] :as channels}]
-  (let [c-router (chan 1)
-        root-el (.getElementById js/document "ui")]
-    (tap ml-router  c-router)
-    (go (loop [state nil]
-          (let [[v port] (alts! [c-router])]
-            (condp = port
-              c-router (let [{:keys [router/handler history/pushed]} v]
-                         (if (= handler :page/games)
-                           (do
-                             #_(println (gstring/format "rendering %s" handler))
-                             (render-page-games root-el channels v)
-                             (recur (merge state v)))
-                           (do (recur state)))))))
-        (println "proc-page-games closing"))))
-
-(defn proc-page-userid-games
-  [{:keys [ml-router ml-http-res] :as channels}]
-  (let [c-router (chan 1)
-        root-el (.getElementById js/document "ui")]
-    (tap ml-router  c-router)
-    (go (loop [state nil]
-          (let [[v port] (alts! [c-router])]
-            (condp = port
-              c-router (let [{:keys [router/handler history/pushed]} v]
-                         (if (= handler :page/userid-games)
-                           (do
-                             #_(println (gstring/format "rendering %s" handler))
-                             (render-page-userid-games root-el channels v)
-                             (recur (merge state v)))
-                           (do (recur state)))))))
-        (println "proc-page-userid-games closing"))))
-
-(defn proc-page-userid
-  [{:keys [ml-router ml-http-res] :as channels}]
-  (let [c-router (chan 1)
-        root-el (.getElementById js/document "ui")]
-    (tap ml-router  c-router)
-    (go (loop [state nil]
-          (let [[v port] (alts! [c-router])]
-            (condp = port
-              c-router (let [{:keys [router/handler history/pushed]} v]
-                         (if (= handler :page/userid)
-                           (do
-                             #_(println (gstring/format "rendering %s" handler))
-                             (render-page-userid root-el channels v)
-                             (recur (merge state v)))
-                           (do (recur state)))))))
-        (println "proc-page-userid closing"))))
-
-(defn proc-page-not-found
-  [{:keys [ml-router ml-http-res] :as channels}]
-  (let [c-router (chan 1)
-        root-el (.getElementById js/document "ui")]
-    (tap ml-router  c-router)
-    (go (loop [state nil]
-          (let [[v port] (alts! [c-router])]
-            (condp = port
-              c-router (let [{:keys [router/handler history/pushed]} v]
-                         (if-not handler
-                           (do
-                             #_(println (gstring/format "rendering %s" handler))
-                             (render-not-found root-el channels v)
-                             (recur (merge state v)))
-                           (do (recur state)))))))
-        (println "proc-page-not-found closing"))))
-
-(defn proc-page-login
-  [{:keys [ml-router ml-http-res] :as channels}]
-  (let [c-router (chan 1)
-        root-el (.getElementById js/document "ui")]
-    (tap ml-router  c-router)
-    (go (loop [state nil]
-          (let [[v port] (alts! [c-router])]
-            (condp = port
-              c-router (let [{:keys [router/handler history/pushed]} v]
-                         (if (= handler :page/login)
-                           (do
-                             #_(println (gstring/format "rendering %s" handler))
-                             (render-page-login root-el channels v)
-                             (recur (merge state v)))
-                           (do (recur state)))))))
-        (println "proc-page-login closing"))))
-
-
-(defn proc-page-game
-  [{:keys [ml-router ml-http-res] :as channels}]
-  (let [c-router (chan 1)
-        root-el (.getElementById js/document "ui")]
-    (tap ml-router  c-router)
-    (go (loop [state nil]
-          (let [[v port] (alts! [c-router])]
-            (condp = port
-              c-router (let [{:keys [router/handler history/pushed]} v]
-                         (if (= handler :page/game)
-                           (do
-                             #_(println (gstring/format "rendering %s" handler))
-                             (render-page-game root-el channels v)
-                             (recur (merge state v)))
-                           (do (recur state)))))))
-        (println "proc-page-game closing"))))
-
-
-#_(defn proc-renderer
-  [{:keys [ml-derived-state-ui] :as channels}]
-  (let [c-dsu (chan 1)
-        root-el (.getElementById js/document "ui")]
-    (tap ml-derived-state-ui  c-dsu)
-    (go (loop []
-          (let [{:keys [router/handler history/pushed] :as v} (<! c-dsu)]
-            (println (gstring/format "rendering %s" handler))
-            (condp = handler
-              :page/events (do
-                             (render/page-events root-el channels v)
-                             (recur))
-              :page/games (do
-                            (render/page-games root-el channels v)
-                            (recur))
-              :page/user-games (do
-                                 (render/page-user-games root-el channels v)
-                                 (recur))
-              :page/userid-games (do
-                                   (render/page-userid-games root-el channels v)
-                                   (recur))
-              :page/userid (do
-                             (render/page-userid root-el channels v)
-                             (recur))
-              (do
-                (render/not-found root-el channels v)
-                (recur)))))
-        (println "closing proc-renderer"))))
-
-
-
-
-#_(defn proc-derived-state-ui
-    [{:keys [ml-router ch-derived-state-ui ml-http-res]}]
-    (let [c-router (chan 1)
-          c-http (chan 1)]
-      (tap ml-router c-router)
-      (tap ml-http-res  c-http)
-      (go (loop [s nil]
-            (if-let [[v port] (alts! [c-router])]
-              (condp = port
-                c-router (let [u (select-keys v [:router/handler :history/pushed])
-                               s (merge s u)]
-                           (println "proc-derived-state-ui" s)
-                           (put! ch-derived-state-ui s)
-
-                           (recur s)))))
-          (println "closing proc-derived-state-ui"))))
-
-
+#_(defn ui-header
+    [channels state]
+    (let [{:keys [history/pushed]} state
+          {:keys [handler]} pushed]
+      [:header {:class "ui-header" :style {:display "flex"}}
+       [:div "starnet"]
+       [:a {:href "/events"} "events"]
+       [:br]
+       [:a {:href "/games"} "games"]
+       [:a {:href "u/games"} "u/games"]
+       [:a {:href "/settings"} "settings"]
+       [:a {:href (gstring/format "/u/%s" (gen/generate gen/string-alphanumeric))} "user/random"]
+       [:a {:href (gstring/format "/non-existing" (gen/generate gen/string-alphanumeric))} "not-found"]]))
