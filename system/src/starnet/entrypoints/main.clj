4b8c29a91a9d1ce26f3b2f533d3b2c5b342bf9aa
seeris <sergei.udris@gmail.com>
2020-04-01 16:29:25 +0300

    build

--- system/src/starnet/entrypoints/main.clj
+++
@@ -1,5 +0,0 @@
-(ns starnet.entrypoints.main
-  (:require [starnet.alpha.main]))
-
-(defn -main  [& args]
-  (apply starnet.alpha.main/-main args))
--- system/src/starnet/entrypoints/main.cljs
+++
@@ -1,7 +0,0 @@
-(ns starnet.entrypoints.main
-  (:require
-   [starnet.alpha.main]))
-
-(defn ^:export main
-  [& args]
-  (apply starnet.alpha.main/main args))
