5a41e9a0f72ef408d7a50bf8cc485e6fae1ebe2c
seeris <sergei.udris@gmail.com>
2020-07-28 18:03:17 +0300

    build

---
+++ src/project/ext/store/meta/src/project/ext/store/meta/api.clj
@@ -0,0 +1,4 @@
+(ns project.ext.store.meta.api)
+
+(defn ext-meta []
+  {:id "github.com/express-in-code-game/lab.weekend-2020-07-23/src/project/ext/store"})
---
+++ src/project/ext/store/proc/src/project/ext/store/proc/main.clj
@@ -0,0 +1,4 @@
+(ns project.ext.store.proc.main
+  (:require
+   [project.ext.store.meta.api :as store.api]))
+
