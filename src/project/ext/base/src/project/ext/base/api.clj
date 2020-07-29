e7fe74622755b6d06f81bc2de978754aa09801c1
seeris <sergei.udris@gmail.com>
2020-07-29 15:44:28 +0300

    build

--- src/project/ext/base/src/project/ext/base/api.clj
+++ src/project/ext/base/src/project/ext/base/api.clj
@@ -1,15 +1,9 @@
 (ns project.ext.base.api
-  (:require 
-   [project.core.protocols :as core.p]
-   [project.ext.base.proc :refer [create-proc-ext]]))
-
-(def channels (let []
-                {}))
-
-(def proc-ext (create-proc-ext channels))
+  (:require
+   [project.core.protocols :as core.p]))
 
 
-(defn mount []
-  (core.p/mount* proc-ext))
+(defn mount [proc]
+  (core.p/mount* proc))
 
 (defn bar [])
--- src/project/ext/base/src/project/ext/base/main.clj
+++ src/project/ext/base/src/project/ext/base/main.clj
@@ -1,10 +1,13 @@
-(ns project.ext.base.proc
+(ns project.ext.base.main
   (:require
    [project.core.protocols :as core.p]
    [project.ext.base.protocols :as base.p]
-   [project.ext.base.proc.render :as proc.render]
+   [project.ext.base.render :as render]
    [project.ext.store.api :as store.api]))
 
+
+(def channels (let []
+                {}))
 
 (defn create-proc
   [channels]
@@ -13,7 +16,11 @@
   (with-meta
     {}
     {`core.p/mount* (fn [_]
-                      (core.p/mount* proc.render/proc-ext)
+                      (core.p/mount* proc-render)
                       (core.p/mount* store.api/proc-ext))
      `core.p/unmount* (fn [_])}))
+
+(def proc-ext (create-proc-ext channels))
+
+
 
