93f02f5221c8db7971d2051acb7912a9826e5f73
seeris <sergei.udris@gmail.com>
2020-07-30 08:32:38 +0300

    build

--- src/project/core/src/project/core.clj
+++ src/project/core/src/project/core.clj
@@ -1,1 +1,20 @@
-(ns project.core)
+(ns project.core
+  (:require
+   [clojure.core.async :as a :refer [<! >!  timeout chan alt! go
+                                     alts!  take! put! mult tap untap
+                                     pub sub sliding-buffer mix admix unmix]]))
+
+
+(defn operation-fn
+  [ops| & {:keys []}]
+  (fn [op opts]
+    (go
+      (if-not (clojure.core.async.impl.protocols/closed? ops|)
+        (let [out| (chan 1)]
+          (put! ops| {:op op :opts opts :out| out|})
+          (if (:timeout opts)
+            (alt!
+              out| ([v] v)
+              (timeout (:timeout opts)) nil)
+            (<! out|)))
+        false))))
