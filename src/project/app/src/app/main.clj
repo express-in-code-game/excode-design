7493f3721655a74e9368acf80d3b37c7f0ad4fba
seeris <sergei.udris@gmail.com>
2020-07-31 15:27:29 +0300

    build: pad

--- src/project/app/src/app/main.clj
+++ src/project/app/src/app/main.clj
@@ -10,7 +10,8 @@
    [project.ext.scenarios.main]
    [project.ext.connect.main]
    [project.ext.server.main]
-   [project.ext.games.main]))
+   [project.ext.games.main]
+   #_[pad.spec1]))
 
 
 (def channels (let []
@@ -53,49 +54,3 @@
   #_(p/mount* proc-main {})
   (prn "main")
   #_(<!! (:loop| proc-main)))
-
-(defmacro tmp1
-  [k m]
-  `~m)
-
-(s/fdef clojure.core/declare
-  :args (s/cat :names (s/* simple-symbol?))
-  :ret any?)
-
-
-(def channel-keys #{:a| :b| :c|})
-(def ops #{:o1 :o2 :o3})
-(def channel-ops {:a| #{:o1 :o2}
-                  :b| #{:o3}})
-(def val-maps {:o1 #{:op :data}
-               :o3 #{:op :data :out|}})
-
-#_(s/fdef tmp1
-  :args (s/cat :channel-key (s/and  #_any? channel-keys)
-               :val-map (s/and (fn [v] (and
-                                        (ops (:op v))))))
-  :ret any?)
-
-(s/fdef tmp1
-  :args (s/and
-         (s/cat :channel-key channel-keys
-                :vals-map #(ops (:op %)))
-         (fn [{:keys [channel-key vals-map] :as argm}]
-           (prn argm)
-           (and
-            ((channel-ops channel-key) (:op vals-map))
-            (clojure.set/subset? (val-maps (:op vals-map)) (set (keys vals-map))))))
-  :ret any?)
-
-(defn tmp2 []
-  (tmp1 :a| {:op :o1 :dat1a 3})
-  #_(declare ""))
-
-(comment
-
-  (tmp1 :a| {:op :o1 :dat1a 3})
-
-  (contains? )
-
-  ;;
-  )
