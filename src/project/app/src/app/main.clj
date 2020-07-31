88d4938fb7c609efc4f300cb8c9cb548d551233b
seeris <sergei.udris@gmail.com>
2020-07-31 15:12:02 +0300

    checkpoint: using s/fdef for macro to compile-time validate fn args; allows to validate channel values in compile time without the need for Vals protocol or multiple namespaces

--- src/project/app/src/app/main.clj
+++ src/project/app/src/app/main.clj
@@ -1,5 +1,9 @@
 (ns app.main
   (:require
+   [clojure.core.async :as a :refer [<! >! <!! timeout chan alt! go close!
+                                     >!! <!! alt!! alts! alts!! take! put! mult tap untap
+                                     thread pub sub sliding-buffer mix admix unmix]]
+   [clojure.spec.alpha :as s]
    [project.core.protocols :as p]
    [project.core]
    [project.ext.base.main]
@@ -8,18 +12,11 @@
    [project.ext.server.main]
    [project.ext.games.main]))
 
-(declare proc-main-f)
 
 (def channels (let []
                 {}))
 
 (def ctx (atom {:exts {}}))
-
-(def proc-main (proc-main-f channels ctx))
-
-(defn -main [& args]
-  (p/mount* proc-main {})
-  #_(<!! (:loop| proc-main)))
 
 (defn proc-main-f
   [channels ctx]
@@ -48,3 +45,57 @@
         p/Mountable
         (p/mount* [_ opts] (put! ops| {:op :mount}))
         (p/unmount* [_ opts] (put! ops| {:op :unmount})))))
+
+
+(def proc-main (proc-main-f channels ctx))
+
+(defn -main [& args]
+  #_(p/mount* proc-main {})
+  (prn "main")
+  #_(<!! (:loop| proc-main)))
+
+(defmacro tmp1
+  [k m]
+  `~m)
+
+(s/fdef clojure.core/declare
+  :args (s/cat :names (s/* simple-symbol?))
+  :ret any?)
+
+
+(def channel-keys #{:a| :b| :c|})
+(def ops #{:o1 :o2 :o3})
+(def channel-ops {:a| #{:o1 :o2}
+                  :b| #{:o3}})
+(def val-maps {:o1 #{:op :data}
+               :o3 #{:op :data :out|}})
+
+#_(s/fdef tmp1
+  :args (s/cat :channel-key (s/and  #_any? channel-keys)
+               :val-map (s/and (fn [v] (and
+                                        (ops (:op v))))))
+  :ret any?)
+
+(s/fdef tmp1
+  :args (s/and
+         (s/cat :channel-key channel-keys
+                :vals-map #(ops (:op %)))
+         (fn [{:keys [channel-key vals-map] :as argm}]
+           (prn argm)
+           (and
+            ((channel-ops channel-key) (:op vals-map))
+            (clojure.set/subset? (val-maps (:op vals-map)) (set (keys vals-map))))))
+  :ret any?)
+
+(defn tmp2 []
+  (tmp1 :a| {:op :o1 :dat1a 3})
+  #_(declare ""))
+
+(comment
+
+  (tmp1 :a| {:op :o1 :dat1a 3})
+
+  (contains? )
+
+  ;;
+  )
