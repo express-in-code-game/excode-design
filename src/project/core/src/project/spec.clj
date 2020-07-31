3e217e24f1eae4c00f18271a2011c8a134a49063
seeris <sergei.udris@gmail.com>
2020-07-31 16:41:40 +0300

    build

--- src/project/core/src/project/spec.clj
+++ src/project/core/src/project/spec.clj
@@ -7,14 +7,19 @@
 (s/def ::op-key #{OP})
 (s/def ::out| any?)
 
-(def channel-keys #{:main/ops|
-                    :game|
-                    :render/ops|})
+
 
 (def ops
   {:app.main/mount (s/keys :req [::op-key ::out|])})
 
-(def op-keys (set (keys ops)))
+(def channels
+  {:main/ops| #{:app.main/mount}
+   :game| #{}
+   :render/ops| #{}})
+
+(def ops-keys (keys ops))
+(def channels-keys (keys channels))
+
 
 ;; (defmulti op-type OP)
 
@@ -25,15 +30,21 @@
 ;; (s/def ::op (s/multi-spec op-type OP))
 ;; 
 
+(s/def ::channel-exists channels-keys)
+(s/def ::op-exists #(ops-keys (OP %)))
+(s/def ::channel-op-exists (fn [{:keys [channel-key val-map]}]
+                             ((channel-key channels) (OP val-map))))
+(s/def ::val-map-valid (fn [{:keys [channel-key val-map]}]
+                         (s/valid? ((OP val-map) ops) val-map)))
+
 (defmacro vl
   [channel-key val-map]
   `~m)
 
 (s/fdef vl
   :args (s/and
-         (s/cat :channel-key channel-keys
-                :val-map #(ops (:op %)))
-         (fn [{:keys [channel-key val-map] :as argm}]
-           
-           ))
+         (s/cat :channel-key ::channel-exists
+                :val-map ::op-exists)
+         ::channel-op-exists
+         ::val-map-valid)
   :ret any?)
