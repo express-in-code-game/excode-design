142ff954eaf6c904635345d9c8b516ed590e2fcd
seeris <sergei.udris@gmail.com>
2020-11-15 16:28:07 +0300

    build: /bin cljs-compiler, dc-dev.yml

---
+++ src/ui/src/deathstar/ui/main.cljs
@@ -0,0 +1,17 @@
+(ns deathstar.ui.main
+  (:require
+   [clojure.core.async :as a :refer [chan go go-loop <! >!  take! put! offer! poll! alt! alts! close!
+                                     pub sub unsub mult tap untap mix admix unmix pipe
+                                     timeout to-chan  sliding-buffer dropping-buffer
+                                     pipeline pipeline-async]]
+   [goog.string :refer [format]]
+   [cljs.reader :refer [read-string]]
+   [clojure.pprint :refer [pprint]]
+
+   [cljctools.csp.op.spec :as op.spec]))
+
+(defn ^:export main
+  []
+  (println ::main))
+
+(do (main))
