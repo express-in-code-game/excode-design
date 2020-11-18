f8a17d9c5f371705bda819319e57334f0fc3ee4e
seeris <sergei.udris@gmail.com>
2020-11-18 11:13:47 +0300

    build peernode: acquiring daemon ref works, have access to ipfs

--- src/peernode/src/deathstar/peernode/main.cljs
+++ src/peernode/src/deathstar/peernode/main.cljs
@@ -16,10 +16,32 @@
    [cljctools.cljc.core :as cljc.core]
 
    [cljctools.rsocket.impl]
-   [cljctools.rsocket.impl.examples-nodejs]))
+   [cljctools.rsocket.examples-nodejs]))
 
 (def fs (node/require "fs"))
 (def path (node/require "path"))
 
-(defn main []
-  (println ::main))
+(def ^:dynamic daemon nil)
+
+(comment
+
+  (type daemon)
+  (js/Object.keys daemon)
+  (go
+    (let [id (<p! (daemon._ipfs.id))]
+      (println (js-keys id))
+      (println (.-id id))
+      (println (format "id is %s" id))))
+
+  ;;
+  )
+
+(defn main [d]
+  (println ::main)
+  (println (js-keys d._ipfs))
+  (set! daemon d))
+
+(def exports #js {:main main})
+
+(when (exists? js/module)
+  (set! js/module.exports exports))
