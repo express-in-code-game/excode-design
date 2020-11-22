e9ec473dd0a8f241f6e836089db5f3044341a107
seeris <sergei.udris@gmail.com>
2020-11-22 13:24:49 +0300

    build: flatten /src, add src/scenario-agent src/scenario-meta and -agent-meta

---
+++ src/scenario-list/src/deathstar/sceanrios_render_list/main.cljs
@@ -0,0 +1,5 @@
+(ns deathstar.scenarios-render-list.main
+  (:require
+   [github.sergeiudris.deathstar-lab.scenarios.rovers.render]))
+
+(do nil)
--- src/scenarios-list/src/deathstar/sceanrios_list/main.clj
+++
@@ -1,5 +0,0 @@
-(ns deathstar.scenarios-list.main
-  (:require
-   [github.sergeiudris.deathstar-lab.scenarios.rovers.main]))
-
-(do nil)
