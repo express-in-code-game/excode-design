8004a573b5b0d0b71a326991f7ec676dd55e9278
Sergei Udris <sergei.udris@gmail.com>
2021-03-02 21:16:23 +0300

    app: lacinia resolvers with core.async

--- src/app/src/deathstar/app/lacinia.clj
+++ src/app/src/deathstar/app/lacinia.clj
@@ -7,7 +7,9 @@
    [io.pedestal.http]
    [clojure.java.io]
    [com.walmartlabs.lacinia.pedestal2 :as lacinia.pedestal2]
+   [com.walmartlabs.lacinia.util :as lacinia.util]
    [com.walmartlabs.lacinia.schema :as lacinia.schema]
+   [com.walmartlabs.lacinia.resolve :as lacinia.resolve]
    [com.walmartlabs.lacinia.parser.schema :as lacinia.parser.schema]))
 
 (def hello-schema
@@ -28,12 +30,37 @@
 ;; From the REPL you can call server/start and server/stop on this service
 #_(defonce runnable-service (io.pedestal.http/create-server service))
 
+(def resolvers
+  {:hello
+   (fn [context args value]
+     "world")
+
+   :helloAsync
+   (fn [context args value]
+     (let [result (lacinia.resolve/resolve-promise)]
+       (go
+         (<! (timeout 2000))
+         (lacinia.resolve/deliver! result "world-async"))
+       result))})
+
+(def schema-opts
+  {:streamers
+   {}
+
+   :scalars
+   {}
+
+   :resolvers
+   {:Query {:hello :hello
+            :helloAsync :helloAsync}}})
+
 (defn start
   [channels]
   (go
     (let [schema (->
                   (slurp (clojure.java.io/resource "lacinia/schema.gql"))
-                  lacinia.parser.schema/parse-schema
+                  (lacinia.parser.schema/parse-schema schema-opts)
+                  (lacinia.util/attach-resolvers resolvers)
                   lacinia.schema/compile)
           service (lacinia.pedestal2/default-service
                     schema
