970a3986980c210a838522ab9e847ac984a72e2d
Sergei Udris <sergei.udris@gmail.com>
2021-03-07 13:41:31 +0300

    app: require <!! >!! in .clj namespaces

--- app/src/deathstar/app/lacinia.clj
+++ app/src/deathstar/app/lacinia.clj
@@ -1,6 +1,6 @@
 (ns deathstar.app.lacinia
   (:require
-   [clojure.core.async :as a :refer [chan go go-loop <! >!  take! put! offer! poll! alt! alts! close!
+   [clojure.core.async :as a :refer [chan go go-loop <! >! <!! >!!  take! put! offer! poll! alt! alts! close!
                                      pub sub unsub mult tap untap mix admix unmix pipe
                                      timeout to-chan  sliding-buffer dropping-buffer
                                      pipeline pipeline-async]]
