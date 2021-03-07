9863b844680bc9d758c030709389d1c29bbe2edf
Sergei Udris <sergei.udris@gmail.com>
2021-03-07 10:26:14 +0300

    Revert "design: no docker, straight binaries"
    This reverts commit 4c0e86072e4e73d27e4a481abc4ba791b4881a87.
    - https://github.com/sergeiudris/deathstar.lab/issues/3#issuecomment-792049340

--- test/test/deathstar/test/main_test.clj
+++ test/test/deathstar/test/main_test.clj
@@ -13,14 +13,14 @@
    [clojure.test.check.properties :as prop]
    [clojure.test :refer [is run-all-tests testing deftest run-tests]]))
 
-#_(defn the-whole-app-fixture
+(defn the-whole-app-fixture
   [f]
   (let [opts (deathstar.app.main/create-opts {:deathstar.app.main/id :test})]
     (a/<!! (deathstar.app.main/mount opts))
     (f)
     (a/<!! (deathstar.app.main/unmount opts))))
 
-#_(clojure.test/use-fixtures :once the-whole-app-fixture)
+(clojure.test/use-fixtures :once the-whole-app-fixture)
 
 (comment
 
