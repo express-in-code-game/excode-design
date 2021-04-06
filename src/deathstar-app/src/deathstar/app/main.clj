f1a7759a33cc6d85755bdbc24df7ad9d7785bfe8
Sergei Udris <sergei.udris@gmail.com>
2021-04-06 11:06:57 +0300

    re-add dgraph

--- src/deathstar-app/src/deathstar/app/main.clj
+++ src/deathstar-app/src/deathstar/app/main.clj
@@ -10,7 +10,9 @@
    [deathstar.app.spec :as app.spec]
 
    [deathstar.app.system-tray]
-   [deathstar.app.reitit]))
+   [deathstar.app.reitit]
+   [deathstar.app.docker-dgraph]
+   [deathstar.app.dgraph]))
 
 (defonce ^:private registry-ref (atom {}))
 
@@ -20,7 +22,10 @@
    ::app.spec/state* (atom {})
    ::channels {::app.spec/system-exit| (chan 1)}
    ::system-tray? false
-   ::port 3080})
+   ::port 3080
+   ::dgraph-opts (deathstar.app.docker-dgraph/create-opts
+                  {:deathstar.app.docker-dgraph/id id
+                   :deathstar.app.docker-dgraph/remove-volume? false})})
 
 (def peer1-preset (create-opts
                    {::id :peer1
@@ -40,6 +45,7 @@
       (when system-tray?
         (<! (deathstar.app.system-tray/unmount {})))
       (<! (deathstar.app.reitit/stop channels {:deathstar.app.reitit/port port}))
+      (<! (deathstar.app.docker-dgraph/down dgraph-opts))
       (let [opts-in-registry (get @registry-ref id)]
         (when (::procs-exit opts-in-registry)
           (<! ((::procs-exit opts-in-registry)))))
@@ -66,6 +72,10 @@
       (when system-tray?
         (<! (deathstar.app.system-tray/mount {:deathstar.app.system-tray/quit| (::app.spec/system-exit| channels)})))
       (<! (deathstar.app.reitit/start channels {:deathstar.app.reitit/port port}))
+      (<! (deathstar.app.docker-dgraph/count-images))
+      (<! (deathstar.app.docker-dgraph/up dgraph-opts))
+      #_(<! (deathstar.app.dgraph/ready?))
+      (<! (deathstar.app.dgraph/upload-schema))
 
       (let [exit| (chan 1)
             proc|
